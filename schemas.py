299b533f2a925ddd7e6c8377caa0e1315dcde76d
khansolo19 <chyngyz.tangatarov.work@gmail.com>
2022-06-25 19:06:35 +0600

    first commit

---
+++ schemas.py
@@ -0,0 +1,19 @@
+from typing import List
+from pydantic import BaseModel
+
+class User(BaseModel):
+    id: int
+    username: str
+
+
+class UploadVideo(BaseModel):
+    title: str
+    description: str 
+    
+
+class GetVideo(BaseModel):
+    user: User
+    video: UploadVideo
+
+class Message(BaseModel):
+    message: str
---
+++ services.py
@@ -0,0 +1,9 @@
+import shutil
+from fastapi import UploadFile
+
+class Video:
+    pass
+
+def write_video(file_name: str, file: UploadFile):
+    with open ('file_name', 'wb')as buffer:
+        shutil.copyfileobj(file.file, buffer)
